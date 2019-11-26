ccb4aa49b72a987613cf21f96d987894467da48a
Ivana <bartolivana@gmail.com>
2019-11-26 10:58:20 +0100

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import FormName from './FormName'
+import CardName from './CardName'
 
 function App() {
+  const [cards, setCards] = useState([])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FormName onSubmit={handleFormSubmit} />
+      {cards.map(card => (
+        <CardName {...card} />
+      ))}
     </div>
-  );
+  )
+
+  function handleFormSubmit(data) {
+    setCards([...cards, data])
+  }
 }
 
-export default App;
+export default App
